735935bf5751e4b186bc1a8081c032db6678e517
Tahul <yael.guilloux@gmail.com>
2026-03-09 00:28:05 +0100

    fix: use img tag for proper scaling, make dithering overlay visible

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <div className="background-image" />
+      <img src="/sublimatio.png" alt="Sublimatio" className="background-image" />
       <div className="dithering-overlay">
         <Dithering
           width="100%"
           height="100%"
-          colorBack="#00000000"
-          colorFront="#000000"
+          colorBack="#0a0a0a"
+          colorFront="#1a1a1a"
           shape="simplex"
           type="4x4"
-          size={2}
-          speed={0.3}
-          scale={0.6}
+          size={3}
+          speed={0.2}
+          scale={0.8}
           fit="cover"
         />
       </div>
